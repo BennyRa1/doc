74d72d5a09725385f2f23ca6a594bac0d1ba3ea4
BennyRa1 <147187628+BennyRa1@users.noreply.github.com>
2024-04-07 00:10:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My LAMP Stack Project</title>
+</head>
+<body>
+    <h1>Welcome to My LAMP Stack Project!</h1>
+    <?php
+    // PHP code example
+    $message = "Hello, LAMP Stack!";
+    echo "<p>$message</p>";
+    ?>
+</body>
+</html>
